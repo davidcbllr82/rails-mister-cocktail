498e2d5bf95efcccd3a9060a462dff324466d8c2
David Caballero <david.caballero@code.berlin>
2019-11-14 18:44:50 +0100

    Routes, controllers and views generated

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,18 @@
+class DosesController < ApplicationController
+  def new; end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @review = Review.new
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'reviews/create'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
+    resources :reviews, only: [:create]
+  end
+
+  resources :doses, only: [:destroy]
 end
