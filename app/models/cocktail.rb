014d9db53359c1e1360201f532c5d1615a5676e1
David Caballero <david.caballero@code.berlin>
2019-11-15 16:04:19 +0100

    Uploader set

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   has_many :reviews
   validates :name, uniqueness: true, presence: true
+  mount_uploader :photo, PhotoUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,24 @@
 25.times do
+  Cocktail.create(
+    name: Faker::Dessert.variety,
+    photo: "https://res.cloudinary.com/hard-to-debug-issues/image/upload/v1573827827/200485_3000x2000_mkbr3o.jpg"
+  )
+end
+
+25.times do
   Ingredient.create(
-    name: Faker::Food.ingredient
+    name: Faker::Dessert.topping
+  )
+end
+
+25.times do
+  Dose.create(
+    description: Faker::Food.measurement
   )
 end
 
 25.times do
   Review.create(
-    content: Faker::Restaurant.review
+    content: Faker::TvShows::StrangerThings.quote
   )
 end
